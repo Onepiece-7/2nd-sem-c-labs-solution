1083e99754ea603271dd8a43570a673c546514c4
One piece <157497306+Onepiece-7@users.noreply.github.com>
2025-05-20 17:46:03 +0545

    Add files via upload

---
+++ lab5/2.cpp
@@ -0,0 +1,9 @@
+// Create c class called employee with data member code, name, address, salary.
+// Create c constructor to initialize the member of the class. Also create the
+// another constructor so that we can create an object from another object. Define member
+// function display() to display the information of the class.
+#include <iostream>
+using namespace std;
+class employee
+{
+};
