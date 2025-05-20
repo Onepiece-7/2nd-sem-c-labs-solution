d6d23d34904c57833e125c3d61b4517127210d0c
One piece <157497306+Onepiece-7@users.noreply.github.com>
2025-05-20 18:16:54 +0545

    Update 2.cpp

--- lab5/2.cpp
+++ lab5/2.cpp
@@ -6,4 +6,52 @@
 using namespace std;
 class employee
 {
+private:
+    int code;
+    string name;
+    string address;
+    float salary;
+
+public:
+    employee(int c, string n, string a, float s) // parameterized constructor
+    {
+        code = c;
+        name = n;
+        address = a;
+        salary = s;
+    }
+    employee(employee &e) // copy constructor
+    {
+        code = e.code;
+        name = e.name;
+        address = e.address;
+        salary = e.salary;
+    }
+    void display()
+    {
+        cout << "Code: " << code << endl;
+        cout << "Name: " << name << endl;
+        cout << "Address: " << address << endl;
+        cout << "Salary: " << salary << endl;
+    }
 };
+int main()
+{
+    int code;
+    string name, address;
+    float salary;
+    cout << "Enter the name of the employee:" << endl;
+    cin >> name;
+    cout << "Enter the specific code of the employee:" << endl;
+    cin >> code;
+    cout << "Where does this employee lives: " << endl;
+    cin >> address;
+    cout << "How much does this employee earn: " << endl;
+    cin >> salary;
+    employee e1(code, name, address, salary);
+    employee e2(e1);
+    e1.display();
+    cout << endl;
+    e2.display();
+    return 0; // creating object using copy constructor
+}
