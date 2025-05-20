2c8519078997a4900051aabd9a410b3a62926c26
One piece <157497306+Onepiece-7@users.noreply.github.com>
2025-05-20 17:34:34 +0545

    Update 1.cpp

--- lab3/1.cpp
+++ lab3/1.cpp
@@ -1,1 +1,42 @@
 
+// WAP o find sum of two complex number by passing object as an argument
+#include <iostream>
+using namespace std;
+class complex
+{
+private:
+    int real;
+    int imag;
+
+public:
+    void get_data();
+    complex addcomplex(complex c2);
+    void display();
+};
+void complex::get_data()
+{
+    cout << " Enter the complex number" << endl;
+    cin >> real >> imag;
+};
+complex complex::addcomplex(complex c2)
+{
+    complex temp;
+    temp.real = real + c2.real;
+    temp.imag = imag + c2.imag;
+    return temp;
+};
+void complex::display()
+{
+    cout << real << "+" << imag << "i" << endl;
+};
+int main()
+{
+    complex c1, c2, c3;
+    cout << "for first complex number" << endl;
+    c1.get_data();
+    cout << "for second complex number" << endl;
+    c2.get_data();
+    c3 = c1.addcomplex(c2);
+    cout << "sum of two complex number=" << endl;
+    c3.display();
+}
