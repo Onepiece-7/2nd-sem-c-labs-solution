e6c9f180270a17ff7116e61e69ab363325407eb5
One piece <157497306+Onepiece-7@users.noreply.github.com>
2025-05-20 17:29:24 +0545

    Update 1.cpp

--- lab1/1.cpp
+++ lab1/1.cpp
@@ -1,1 +1,8 @@
 
+// WAP to display "Welcome to everest engineering college" in c++
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "welcome to everest engineering college";
+}
